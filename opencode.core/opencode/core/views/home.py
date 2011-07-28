80467af223b85ff557a060f7ee3ab34b4d926796
Bernard Chhun <bernard.chhun@gmail.com>
2011-07-28 06:04:23 -0400

    la view principal

---
+++ opencode.core/opencode/core/views/home.py
@@ -0,0 +1,10 @@
+#coding: utf8
+
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.conf import settings
+
+def home(request):
+    data = {}
+    return render_to_response("accueil.html", data, context_instance=RequestContext)
